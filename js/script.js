de9005ab5c6570973a5c6994339dda59b7fd866f
Shirlette Chambers <shirlette.chambers@gmail.com>
2016-08-07 15:36:54 -0400

    Functional buttons added for different categories

--- js/script.js
+++ js/script.js
@@ -6,47 +6,91 @@ var apiBaseUrl = "http://api.themoviedb.org/3/"
 var imageBaseUrl = 'http://image.tmdb.org/t/p/';
 // the query string, including the apiKey
 var apiKey = '?api_key=fec8b5ab27b292a68294261bb21b04a5';
-var npUrl = apiBaseUrl + 'movie/popular' + apiKey;
-var topUrl = apiBaseUrl + 'search/multi' + apiKey;
 
-	$.getJSON(npUrl, function(nowPlayingData) {
+var latestUrl = apiBaseUrl + 'movie/latest' + apiKey;
+var npUrl = apiBaseUrl + 'movie/now_playing' + apiKey;
+var popUrl = apiBaseUrl + 'movie/popular' + apiKey;
+var rateUrl = apiBaseUrl + 'movie/top_rated' + apiKey;
+var upUrl = apiBaseUrl + 'movie/upcoming' + apiKey;
+
+var searchUrl = apiBaseUrl + 'search/multi' + apiKey;
+console.log(latestUrl);
+
+
+$('.latest').click(function(){
+		showMovieType();
+	});
+
+function showMovieType(){
+	if('latestUrl'){
+			$.getJSON(latestUrl, function(nowPlayingData) {
 		console.log(nowPlayingData);
-		var npHTML = '';
+		var latestHTML = '';
 		for(var i = 0; i < nowPlayingData.results.length; i++){
-			npHTML += '<div class="col">';
-			npHTML += nowPlayingData.results[i].title;
-			var overviewUrl = imageBaseUrl + 'w300' + nowPlayingData.results[i].overview;
+			latestUrl += '<div class="col">';
+			latestUrl += nowPlayingData.results[i].title;
+			var overviewUrl = apiBaseUrl + 'w300' + nowPlayingData.results[i].overview;
 			var posterUrl = imageBaseUrl + 'w300' + nowPlayingData.results[i].poster_path;
-			npHTML += '<a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal"><img src="' + posterUrl + '"></a>';
-			npHTML += '</div>';
+			latestUrl += '<a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal"><img src="' + posterUrl + '"></a>';
+			latestUrl += '</div>';
 
 		}
-		$('.poster-grid').html(npHTML);
+		$('.poster-grid').html(latestUrl);
 	})
 
-	$.getJSON(topUrl, function(top20set) {
-		console.log(top20set);
-		$('.search').click(function(){
+	}
+}
+	// $.getJSON(npUrl, function(nowPlayingData) {
+	// 	console.log(nowPlayingData);
+	// 	var npHTML = '';
+	// 	for(var i = 0; i < nowPlayingData.results.length; i++){
+	// 		npHTML += '<div class="col">';
+	// 		npHTML += nowPlayingData.results[i].title;
+	// 		var overviewUrl = apiBaseUrl + 'w300' + nowPlayingData.results[i].overview;
+	// 		var posterUrl = imageBaseUrl + 'w300' + nowPlayingData.results[i].poster_path;
+	// 		npHTML += '<a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal"><img src="' + posterUrl + '"></a>';
+	// 		npHTML += '</div>';
+
+	// 	}
+	// 	$('.poster-grid').html(npHTML);
+	// })
+
+	$.getJSON(searchUrl, function(top20set) {
+
+		$('.click-event').click(function(){
 		search();
+		console.log("top20set worked");
+
+
+		$('.click-event').submit(function(){
+			e.preventDefault();
+			Search(this);
+			console.log("submit function worked");
+		});
 	
 		var topHTML = '';
 		for(var i = 0; i < top20set.results.length; i++){
 			topHTML += '<div class="col">';
-			topHTML += nowPlayingData.results[i].title;
-			var posterUrl = imageBaseUrl + 'w300' + nowPlayingData.results[i].poster_path;
+			topHTML += top20set.results[i].title;
+			var posterUrl = imageBaseUrl + 'w300' + top20set.results[i].poster_path;
+			// if( topHTML.name[i] === input && topHTML. === drink ) {
+   //          result.push( el );
+   // 			console.log(top20set);
+   //      }
 			topHTML += '<a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal"><img src="' + posterUrl + '"></a>';
 			topHTML += '</div>';
 
-		}
+		
 		$('.poster-grid').html(topHTML);
 	})
 });
 
-	$('#myModal').reveal({
-	     animation: 'fadeAndPop',                   //fade, fadeAndPop, none
-	     animationspeed: 300,                       //how fast animtions are
-	     closeonbackgroundclick: true,              //if you click background will modal close?
-	     dismissmodalclass: 'close-reveal-modal'    //the class of a button or element that will close an open modal
-	});
+	// $('#myModal').reveal({
+	//      animation: 'fadeAndPop',                   //fade, fadeAndPop, none
+	//      animationspeed: 300,                       //how fast animtions are
+	//      closeonbackgroundclick: true,              //if you click background will modal close?
+	//      dismissmodalclass: 'close-reveal-modal'    //the class of a button or element that will close an open modal
+	// });
 
-});
+// });
+

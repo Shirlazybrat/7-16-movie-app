743424024db4b64ec5d3c71bc815f8b38ca1b699
Shirlette Chambers <shirlette.chambers@gmail.com>
2016-08-07 19:54:15 -0400

    Commands added to each button for movie info update

--- js/script.js
+++ js/script.js
@@ -17,73 +17,125 @@ var searchUrl = apiBaseUrl + 'search/multi' + apiKey;
 console.log(latestUrl);
 
 
-$('.latest').click(function(){
-		showMovieType();
+$('.latest').click(function(latestData){
+	alert("button clicked");
+		showLateMovieType();
 	});
 
-function showMovieType(){
+function showLateMovieType(){
 	if('latestUrl'){
-			$.getJSON(latestUrl, function(nowPlayingData) {
-		console.log(nowPlayingData);
+			$.getJSON(latestUrl, function(latestData) {
+		console.log(latestData);
 		var latestHTML = '';
-		for(var i = 0; i < nowPlayingData.results.length; i++){
+		for(var i = 0; i < latestData.results.length; i++){
 			latestUrl += '<div class="col">';
-			latestUrl += nowPlayingData.results[i].title;
-			var overviewUrl = apiBaseUrl + 'w300' + nowPlayingData.results[i].overview;
-			var posterUrl = imageBaseUrl + 'w300' + nowPlayingData.results[i].poster_path;
+			latestUrl += latestData.results[i].title;
+			var overviewUrl = apiBaseUrl + 'w300' + latestData.results[i].overview;
+			var posterUrl = imageBaseUrl + 'w300' + latestData.results[i].poster_path;
 			latestUrl += '<a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal"><img src="' + posterUrl + '"></a>';
 			latestUrl += '</div>';
 
 		}
 		$('.poster-grid').html(latestUrl);
-	})
+		})
 
 	}
 }
-	// $.getJSON(npUrl, function(nowPlayingData) {
-	// 	console.log(nowPlayingData);
-	// 	var npHTML = '';
-	// 	for(var i = 0; i < nowPlayingData.results.length; i++){
-	// 		npHTML += '<div class="col">';
-	// 		npHTML += nowPlayingData.results[i].title;
-	// 		var overviewUrl = apiBaseUrl + 'w300' + nowPlayingData.results[i].overview;
-	// 		var posterUrl = imageBaseUrl + 'w300' + nowPlayingData.results[i].poster_path;
-	// 		npHTML += '<a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal"><img src="' + posterUrl + '"></a>';
-	// 		npHTML += '</div>';
 
-	// 	}
-	// 	$('.poster-grid').html(npHTML);
-	// })
+$.getJSON(npUrl, function(nowPlayingData) {
+		console.log(nowPlayingData);
+		var npHTML = '';
+		for(var i = 0; i < nowPlayingData.results.length; i++){
+			npHTML += '<div class="col">';
+			npHTML += nowPlayingData.results[i].title;
+			var overviewUrl = apiBaseUrl + 'w300' + nowPlayingData.results[i].overview;
+			var posterUrl = imageBaseUrl + 'w300' + nowPlayingData.results[i].poster_path;
+			npHTML += '<a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal"><img src="' + posterUrl + '"></a>';
+			npHTML += '</div>';
 
-	$.getJSON(searchUrl, function(top20set) {
-
-		$('.click-event').click(function(){
-		search();
-		console.log("top20set worked");
+		}
+		$('.poster-grid').html(npHTML);
+	})
 
 
-		$('.click-event').submit(function(){
-			e.preventDefault();
-			Search(this);
-			console.log("submit function worked");
-		});
+
+$('.popular').click(function(popularData){
+	alert("button clicked");
+		showPopMovieType();
+	});
+
+function showPopMovieType(){
+	if('popUrl'){
+			$.getJSON(popUrl, function(popularData) {
+		console.log(popularData);
+		var popularHTML = '';
+		for(var i = 0; i < popularData.results[i].length; i++){
+			popUrl += '<div class="col">';
+			popUrl += popularData.results[i].title;
+			var overviewUrl = apiBaseUrl + 'w300' + popularData.results[i].overview;
+			var posterUrl = imageBaseUrl + 'w300' + popularData.results[i].poster_path;
+			popUrl += '<a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal"><img src="' + posterUrl + '"></a>';
+			popUrl += '</div>';
+
+		}
+		$('.poster-grid').html(popUrl);
+		})
+
+	}
+}
+
+
+$('.top-rated').click(function(rateData){
+	alert("button clicked");
+		showTopMovieType();
+	});
+
+function showTopMovieType(){
+	if('rateUrl'){
+			$.getJSON(rateUrl, function(rateData) {
+		console.log(rateData);
+		var rateHTML = '';
+		for(var i = 0; i < rateData.results[i].length; i++){
+			rateUrl += '<div class="col">';
+			rateUrl += rateData.results[i].title;
+			var overviewUrl = apiBaseUrl + 'w300' + rateData.results[i].overview;
+			var posterUrl = imageBaseUrl + 'w300' + rateData.results[i].poster_path;
+			rateUrl += '<a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal"><img src="' + posterUrl + '"></a>';
+			rateUrl += '</div>';
+
+		}
+		$('.poster-grid').html(rateUrl);
+		})
+
+	}
+}
+
 	
-		var topHTML = '';
-		for(var i = 0; i < top20set.results.length; i++){
-			topHTML += '<div class="col">';
-			topHTML += top20set.results[i].title;
-			var posterUrl = imageBaseUrl + 'w300' + top20set.results[i].poster_path;
-			// if( topHTML.name[i] === input && topHTML. === drink ) {
-   //          result.push( el );
-   // 			console.log(top20set);
-   //      }
-			topHTML += '<a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal"><img src="' + posterUrl + '"></a>';
-			topHTML += '</div>';
+// 	$.getJSON(searchUrl, function(top20set) {
+
+// 		$('.click-event').click(function(){
+// 		search();
+// 		console.log("top20set worked");
+	
+// 		});
+
+// 		var topHTML = '';
+// 		for(var i = 0; i < top20set.results.length; i++){
+// 			topHTML += '<div class="col">';
+// 			topHTML += top20set.results[i].title;
+// 			var posterUrl = imageBaseUrl + 'w300' + top20set.results[i].poster_path;
+// 			// if( topHTML.name[i] === input && topHTML. === drink ) {
+//    //          result.push( el );
+//    // 			console.log(top20set);
+//    //      }
+// 			topHTML += '<a href="#" data-reveal-id="myModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal"><img src="' + posterUrl + '"></a>';
+// 			topHTML += '</div>';
 
 		
-		$('.poster-grid').html(topHTML);
-	})
-});
+// 		$('.poster-grid').html(topHTML);
+// 	}
+// });
+
 
 	// $('#myModal').reveal({
 	//      animation: 'fadeAndPop',                   //fade, fadeAndPop, none
@@ -92,5 +144,5 @@ function showMovieType(){
 	//      dismissmodalclass: 'close-reveal-modal'    //the class of a button or element that will close an open modal
 	// });
 
-// });
+});
 
